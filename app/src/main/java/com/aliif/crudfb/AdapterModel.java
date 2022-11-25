2f9c1505f4fc0bba47fccf85ba50c1c5b3d2f071
aliifam <aliif.arief.m@gmail.com>
2022-11-26 01:42:30 +0700

    feat: edit & delete

--- app/src/main/java/com/aliif/crudfb/AdapterModel.java
+++ app/src/main/java/com/aliif/crudfb/AdapterModel.java
@@ -2,15 +2,23 @@ package com.aliif.crudfb;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -43,6 +51,49 @@ public class AdapterModel extends RecyclerView.Adapter<AdapterModel.ModelViewHol
         final Model model = modelList.get(position);
         holder.nama.setText("Nama: "+ model.getNama());
         holder.matkul.setText("Matkul: " + model.getMatkul());
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        databaseReference.child("Mahasiswa").child(model.getKey()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(activity, "Data Successfully deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(activity, "Delete data Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).setMessage("Yakin mau dihapus? " + model.getNama());
+                builder.show();
+            }
+        });
+
+        holder.cardView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                FragmentManager fragmentManager = ((AppCompatActivity)activity).getSupportFragmentManager();
+                EditForm editForm = new EditForm(
+                        model.getNama(),
+                        model.getMatkul(),
+                        model.getKey(),
+                        "Edit"
+                );
+                editForm.show(fragmentManager, "Form");
+                return true;
+            }
+        });
     }
 
     @Override
@@ -53,11 +104,13 @@ public class AdapterModel extends RecyclerView.Adapter<AdapterModel.ModelViewHol
     public class ModelViewHolder extends RecyclerView.ViewHolder {
         TextView nama, matkul;
         CardView cardView;
+        ImageView imageView;
         public ModelViewHolder(@NonNull View itemView) {
             super(itemView);
             nama = itemView.findViewById(R.id.nama);
             matkul = itemView.findViewById(R.id.matkul);
             cardView = itemView.findViewById(R.id.kartu);
+            imageView = itemView.findViewById(R.id.delete);
         }
     }
 }
---
+++ app/src/main/java/com/aliif/crudfb/EditForm.java
@@ -0,0 +1,79 @@
+package com.aliif.crudfb;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class EditForm extends DialogFragment {
+
+    String nama, matkul, key, choose;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+    public EditForm(String nama, String matkul, String key, String choose)
+    {
+        this.nama = nama;
+        this.matkul = matkul;
+        this.key = key;
+        this.choose = choose;
+    }
+
+    TextView tnama, tmatkul;
+    Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.activity_add, container, false);
+        tnama = view.findViewById(R.id.input_nama);
+        tmatkul = view.findViewById(R.id.input_matkul);
+        button = view.findViewById(R.id.add_button);
+
+        tnama.setText(nama);
+        tmatkul.setText(matkul);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = tnama.getText().toString();
+                String matkul = tmatkul.getText().toString();
+                if (choose.equals("Edit"))
+                {
+                    databaseReference.child("Mahasiswa").child(key).setValue(new Model(nama, matkul)).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(view.getContext(), "Data Successfully Edited", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(view.getContext(), "Data Failed Edited", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+    }
+}
